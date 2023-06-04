9ca6eaf880af276918bebed9c19f89c400628ac5
wuzheng <1657955621@qq.com>
2023-06-05 00:24:37 +0800

    封装v-loading指令

---
+++ src/directives/loading/index.js
@@ -0,0 +1,29 @@
+import loadingImgUrl from '@/assets/loading.svg'
+import styles from './style.module.styl'
+// 创建loading图片元素
+const createLoadingImg = () => {
+  const img = document.createElement('img')
+  img.dataset.role = 'loading'
+  img.src = loadingImgUrl
+  img.classList.add(styles.loading)
+  return img
+}
+
+// v-loading执行的函数，bind和update钩子运行时，执行的逻辑相同，可以直接写成一个函数
+export default function (el, binding) {
+  // 查找当前绑定指令的dom元素内是否存在loading图片元素
+  const loadingImg = el.querySelector('img[data-role=loading]')
+  // loading指令的绑定值为true，并且绑定指令的元素内部没有loading图片元素,就创建laoding图片插入该元素
+  if (binding.value && !loadingImg) {
+    // 当绑定指令的元素的position是默认值时，添加一个类名，样式是position: relative
+    if (getComputedStyle(el).position === 'static') {
+      el.classList.add(styles.parent)
+    }
+    el.append(createLoadingImg())
+  }
+  // loading指令绑定的值为false时，如果绑定指定的元素内部有loading图片元素就移除它,同时移除给父元素添加的的类名
+  if (!binding.value) {
+    loadingImg && loadingImg.remove()
+    el.classList.remove(styles.parent)
+  }
+}
--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import './styles/gloab.styl'
 import { showMessage } from '@/utils/index'
 // 导入mock
 import './mock'
+import vLoading from '@/directives/loading'
 // 将展示消息的方法挂载在vue的原型上
 Vue.prototype.$showMessage = showMessage
 Vue.config.productionTip = false
+// 注册全局指令v-loading
+Vue.directive('loading', vLoading)
 
 new Vue({
   render: h => h(App),
